f0709bc933105d9a4cef74aac7aa7de87d7a13f4
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2023-03-29 21:58:29 -0300

    fix insert token and create print token function

--- src/utils/tokens_list.h
+++ src/utils/tokens_list.h
@@ -1,41 +1,41 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "../../y.tab.h"
 #define MAX_SYMBOLS 1000
 
 typedef struct {
-    yytokentype type;
-    char* word
+    yytoken_kind_t type;
+    char word[32]; // allocate space for the token word
 } token_t;
 
-char* tokens[MAX_SYMBOLS];
+token_t tokens[MAX_SYMBOLS];
 int num_tokens = 0;
 
-//char* lookup_token(char* symbol);
-
-void insert_token(token_t yytoken) {
+void insert_token(yytoken_kind_t type, char* word) {
     if (num_tokens >= MAX_SYMBOLS) {
         printf("Error: symbol table overflow\n");
         return;
     }
-    tokens[num_tokens] = new_token;
+    tokens[num_tokens].type = type;
+    strcpy(tokens[num_tokens].word, word);
     num_tokens++;
 }
-/*
-char* lookup_token(char* symbol) {
+
+token_t* lookup_token(char* symbol) {
     int i;
     for (i = 0; i < num_tokens; i++) {
-        if (strcmp(tokens[i], symbol) == 0) {
-            return tokens[i];
+        if (strcmp(tokens[i].word, symbol) == 0) {
+            return &tokens[i];
         }
     }
     return NULL;
-}*/
+}
 
 void print_tokens() {
     int i;
     printf("Tokens\n");
     for (i = 0; i < num_tokens; i++) {
-        printf("%s\n", tokens[i]);
+        printf("<%d, %s>\n", tokens[i].type, tokens[i].word);
+        continue;
     }
 }
